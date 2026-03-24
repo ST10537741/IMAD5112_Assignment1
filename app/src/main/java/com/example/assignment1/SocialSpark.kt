8d15290faddff7ce6800a3b7c0e03c7d341cf7ff
Thulasizwe Myambo <ST10537741@myemeris.edu.za>
2026-03-24 20:13:01 +0200

    Val Commit
    Added Variables for the different elements of the app.

--- app/src/main/java/com/example/assignment1/SocialSpark.kt
+++ app/src/main/java/com/example/assignment1/SocialSpark.kt
@@ -1,4 +1,24 @@
 package com.example.assignment1
 
-class SocialSpark {
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+import android.widget.EditText
+class SocialSpark : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.spark_social) // points to the spark_social XML
+
+        val socialSParkEditText = findViewById<EditText>(R.id.socialSparkEditText)
+        val confirmButton = findViewById<Button>(R.id.confirmButton)
+        val resetButton = findViewById<Button>(R.id.resetButton)
+        val socialSparkTextView = findViewById<TextView>(R.id.socialSparkTextView)
+
+        
+
+
+
+    }
 }
