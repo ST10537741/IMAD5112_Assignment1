c955443d318e5db2d9968e6811bb2b7ed56c6301
Thulasizwe Myambo <ST10537741@myemeris.edu.za>
2026-03-24 19:11:12 +0200

    2nd Commit
    Same as Initial commit

---
+++ app/src/main/java/com/example/assignment1/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.assignment1
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val getStartedButton = findViewById<Button>(R.id.getStartedButton)
+        val welcomeTextView = findViewById<TextView>(R.id.welcomeTextView)
+
+        getStartedButton.setOnClickListener {
+// Create an intent to move from activity 1 to activity 2
+            val intent = Intent(this, SocialSpark::class.java)
+            startActivity(intent)
+
+
+        }
+    }
+}
